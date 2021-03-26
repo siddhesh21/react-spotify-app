508e75c3ebac63b92cb482b36a77b0522ca64e49
Siddhesh Kankekar <kankekas@tcd.ie>
2021-03-26 20:58:48 +0000

    Styled Footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,8 +1,49 @@
 import React from "react";
 import "./Footer.css";
+import PlayCircleOutlineIcon from "@material-ui/icons/PlayCircleOutline";
+import SkipPreviousIcon from "@material-ui/icons/SkipPrevious";
+import SkipNextIcon from "@material-ui/icons/SkipNext";
+import ShuffleIcon from "@material-ui/icons/Shuffle";
+import RepeatIcon from "@material-ui/icons/Repeat";
+import PlaylistPlayIcon from "@material-ui/icons/PlaylistPlay";
+import VolumeDownIcon from "@material-ui/icons/VolumeDown";
+import { Grid, Slider } from "@material-ui/core";
 
 function Footer() {
-  return <div className="footer">FOOTER</div>;
+  return (
+    <div className="footer">
+      <div className="footer__left">
+        <img
+          src="https://i.scdn.co/image/ab67706f00000003c414e7daf34690c9f983f76e"
+          alt="Lo-Fi Beats"
+          className="footer__albumLogo"
+        />
+        <div className="footer__songInfo">Lo-Fi Beats</div>
+      </div>
+
+      <div className="footer__center">
+        <ShuffleIcon className="footer__green" />
+        <SkipPreviousIcon className="footer__icon" />
+        <PlayCircleOutlineIcon fontSize="large" className="footer__green" />
+        <SkipNextIcon className="footer__icon" />
+        <RepeatIcon className="footer__green" />
+      </div>
+
+      <div className="footer__right">
+        <Grid container spacing={2}>
+          <Grid item>
+            <PlaylistPlayIcon />
+          </Grid>
+          <Grid item>
+            <VolumeDownIcon />
+          </Grid>
+          <Grid item xs>
+            <Slider />
+          </Grid>
+        </Grid>
+      </div>
+    </div>
+  );
 }
 
 export default Footer;
