17d4b62e823f846845a2eaa6d7361c800cda00b4
Siddhesh Kankekar <kankekas@tcd.ie>
2021-03-27 13:34:39 +0000

    Added last body + songs component

--- src/App.js
+++ src/App.js
@@ -36,6 +36,12 @@ function App() {
           playlists: playlists,
         });
       });
+      spotify.getPlaylist('37i9dQZF1DWWQRwui0ExPn').then((response) => {
+        dispatch({
+          type: "SET_DISCOVER_WEEKLY",
+          discover_weekly: response,
+        });
+      });
     }
   }, [token, dispatch]);
 
